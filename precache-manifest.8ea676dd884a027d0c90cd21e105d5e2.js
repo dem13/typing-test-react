08ff5ac7f8e4c5e0a7465b424995c333159b8237
demyansson <idcadifi@gmail.com>
2020-07-02 09:51:11 +0300

    Updates

---
+++ precache-manifest.8ea676dd884a027d0c90cd21e105d5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3909d8dc87263af127919300be1d1763",
+    "url": "/typing-test-react/index.html"
+  },
+  {
+    "revision": "876db1e0494376c5834f",
+    "url": "/typing-test-react/static/css/main.d9930ddb.chunk.css"
+  },
+  {
+    "revision": "1081df5f4f7ac429de8e",
+    "url": "/typing-test-react/static/js/2.5f963253.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing-test-react/static/js/2.5f963253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876db1e0494376c5834f",
+    "url": "/typing-test-react/static/js/main.dc5e3fab.chunk.js"
+  },
+  {
+    "revision": "b7fa71921b2bdc63a8d5",
+    "url": "/typing-test-react/static/js/runtime-main.962fb64a.js"
+  }
+]);
